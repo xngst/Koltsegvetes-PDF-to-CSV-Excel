78207776b371cf3c4bfd2fc7e4853b5f9f843d6a
Aron Feher <tarcvzf@protonmail.ch>
2023-03-28 14:04:39 +0200

    Update pdf_2_csv_install.bat

--- install/pdf_2_csv_install.bat
+++ install/pdf_2_csv_install.bat
@@ -4,7 +4,7 @@
 @echo MKKP pdf to csv install
 @echo ----------------------------------------------
 
-pip install tabula --trusted-host files.pythonhosted.org
+pip install tabula-py --trusted-host files.pythonhosted.org
 pip install pandas --trusted-host files.pythonhosted.org
 pip install openpyxl --trusted-host files.pythonhosted.org
 
